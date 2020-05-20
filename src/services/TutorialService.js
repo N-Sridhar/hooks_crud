0bb118d049beb83a6cb6ce3920731ad95af70b10
N-Sridhar <nsridharbtech@gmail.com>
2020-05-20 17:44:31 +0530

    crud url's

---
+++ src/services/TutorialService.js
@@ -0,0 +1,27 @@
+import http from "../http-common";
+
+const getAll = () => {
+  return http.get("/tutorials");
+};
+
+const get = (id) => {
+  return http.get(`/tutorials/${id}`);
+};
+
+const create = (data) => {
+  return http.post("/tutorials", data);
+};
+
+const update = (id, data) => {
+  return http.put(`/tutorials/${id}`, data);
+};
+
+const remove = (id) => {
+  return http.delete(`/tutorials/${id}`);
+};
+
+const findByName = (name) => {
+  return http.get(`/tutorials?name=${id}`);
+};
+
+export default { get, getAll, create, update, remove, findByName };
